bb1e3bf1f0a0165e121f3b78a731867c9045b477
hamso <hamsonassor14@gmail.com>
2022-04-28 09:39:43 +0300

    modified views and urls

--- pollster/polls/views.py
+++ pollster/polls/views.py
@@ -12,7 +12,7 @@ def index(request):
     return render(request, 'polls/index.html', context)
 
 # Show specific question and choices
-def poll_detail(request, question_id):
+def detail(request, question_id):
   try:
     question = Question.objects.get(pk=question_id)
   except Question.DoesNotExist:
